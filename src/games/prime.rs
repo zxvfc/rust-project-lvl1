10ee2292c09e47fa5211207eba27229706c6e9a3
Valentin Isipchuk <velya.96@gmail.com>
2025-12-06 21:54:57 +0700

    Refacto prime

--- src/games/prime.rs
+++ src/games/prime.rs
@@ -5,26 +5,26 @@ const RULES: &str = "Answer 'yes' if given number is prime. Otherwise answer 'no
 pub fn get_data() -> GameData {
     let questions_and_answers = std::array::from_fn(|_| {
         let num = get_rand();
-        let answer = if is_prime(num as f64) { "yes" } else { "no" };
+        let answer = if is_prime(num) { "yes" } else { "no" };
         (num.to_string(), answer.to_string())
     });
     (RULES.to_string(), questions_and_answers)
 }
 
-fn is_prime(num: f64) -> bool {
-    if num == 2.0 || num == 3.0 {
-        return true;
-    }
-    if num <= 1.0 || num % 2.0 == 0.0 || num % 3.0 == 0.0 {
-        return false;
-    }
-    let mut i = 5.0;
-    let sqrt = (num as f64).sqrt();
-    while i <= sqrt {
-        if num % i == 0.0 || num % (i + 2.0) == 0.0 {
-            return false;
+fn is_prime(num: u32) -> bool {
+    match num {
+        0 | 1 => false,
+        2 | 3 => true,
+        n if n % 2 == 0 || n % 3 == 0 => false,
+        n => {
+            let mut i = 5;
+            while i * i <= n {
+                if n % i == 0 || n % (i + 2) == 0 {
+                    return false;
+                }
+                i += 6;
+            }
+            true
         }
-        i += 6.0;
     }
-    true
 }
